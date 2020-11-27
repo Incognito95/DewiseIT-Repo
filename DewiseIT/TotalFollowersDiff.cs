ea2c3963c014c80451b8cbc58908b942d14066a3
incognito95 <4968943@rts-365.dk>
2020-11-27 12:03:11 +0100

    changed named of variables to localTwitter

--- DewiseIT/TotalFollowersDiff.cs
+++ DewiseIT/TotalFollowersDiff.cs
@@ -7,21 +7,18 @@ namespace DewiseIT
         public void TotalFollowersDiffSingleDigit()
         {
             double onlineTwitter = 86327209273.6; // online twitter followers
-            uint fileTwitter = 4044973738; // file twitter followers
+            uint localTwitter = 4044973738; // local twitter followers
 
             // log total of difference to console
-            var totalFollowersDiff = onlineTwitter + fileTwitter;
+            var totalFollowersDiff = onlineTwitter + localTwitter;
 
             // log total difference in decimal number to console
             Console.WriteLine("Total followers difference in decimal number is: " +  totalFollowersDiff);
             
             // round total difference to the nearest whole number
             Console.WriteLine("Total followers difference in whole number is: " + Math.Round(11.6, 0));
-
-
-
-
-
+            
+            
         }
     }
 }
