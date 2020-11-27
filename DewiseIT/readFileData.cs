cb9383edcb6aab912180274f8b55b0be3d211f49
Daniel <35996745+Incognito95@users.noreply.github.com>
2020-11-27 11:12:41 +0100

    Update readFileData.cs

--- DewiseIT/readFileData.cs
+++ DewiseIT/readFileData.cs
@@ -12,10 +12,11 @@ namespace DewiseIT
       
                 Console.WriteLine("Reading json line by line");
                 StreamReader sr = new StreamReader("../../../data.json"); // file path
-
-                // loop through the file
+                
+                // read files to the end
                 string line = sr.ReadToEnd(); // returns string
 
+                // loop through the file
                 foreach (var i in line)
                 {
                     Console.WriteLine(line);
